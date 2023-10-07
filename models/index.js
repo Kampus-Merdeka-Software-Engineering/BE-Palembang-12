510d49487156a371ead8475e523a242058e468fa
Ekoprastyo08 <eko110820@gmail.com>
2023-10-08 05:55:32 +0700

    BIKIN BE ULANG REF KAK NIPE

--- models/index.js
+++
@@ -1,2 +0,0 @@
-export {default as ulasan} from "./ulasan.js";
-
--- models/ulasan.js
+++
@@ -1,9 +0,0 @@
-import { DataTypes } from "sequelize";
-import sequelize from "../config/sequelize.js";
-
-const ulasan = sequelize.define("ulasan", {
-    nama: DataTypes.STRING,
-    isi: DataTypes.STRING
-});
-
-export default ulasan;
